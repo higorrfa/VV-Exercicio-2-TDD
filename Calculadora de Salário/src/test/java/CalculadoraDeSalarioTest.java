bc216e4ff3cb893ba07ec7c93489b36286920775
Higor Araujo <higor.araujo@virtus.ufcg.edu.br>
2022-10-20 22:13:03 -0300

    Caso de teste calculaSalarioDesenvolvedor() com erro por não existir a classe Funcionario

---
+++ Calculadora de Salário/src/test/java/CalculadoraDeSalarioTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CalculadoraDeSalarioTest {
+
+    @Test
+    public void calculaSalarioDesenvolvedor() {
+        double salarioBase = 5000.00;
+        Funcionario funcionario = new Funcionario("Jose", "jose@email.com", salarioBase, Cargo.DESENVOLVEDOR);
+        Assertions.assertEquals(this.calculadora.calcularSalario(funcionario), 4000.00);
+    }
+}
