b92ba6cfe7a0fd4d00cc2f904d4a6ae1fec985c4
lucasafs <lucas.abrantes.silva@ccc.ufcg.edu.br>
2022-10-23 13:35:28 -0300

    Fix testes e add FiltroTest

--- filtroDeFaturas/src/test/java/filtroDeFaturas/FaturaTest.java
+++ filtroDeFaturas/src/test/java/filtroDeFaturas/FaturaTest.java
@@ -1,21 +1,45 @@
 package filtroDeFaturas;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Calendar;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FaturaTest {
+	
+	Calendar data = Calendar.getInstance();
 
 	@Test
     public void criarFaturaComDadosCorretos() {
-        Calendar data = Calendar.getInstance();
         data.set(2022, 5, 10);
         Fatura fatura = new Fatura("6503d", 650.40, data);
 
         Assertions.assertEquals(fatura.getValor(), 650.40);
+        Assertions.assertEquals(fatura.getCodigo(), "6503d");
+        Assertions.assertEquals(fatura.getData(), data);
     }
 
+    @Test()
+	public void criarFaturaComValorNegativo() {
+		data.set(2022, 6, 10);
+		Fatura fatura = new Fatura("6503d", -650.40, data);
+		
+		try {
+			fatura.checaValor(fatura.getValor());
+		} catch (RuntimeException e) {
+			Assertions.assertEquals("Valor negativo!", e.getMessage());
+		}
+    }
+
+    @Test()
+	public void criarFaturaComValorNulo() {
+		data.set(2022, 6, 10);
+		Fatura fatura = new Fatura("6503d", null, data);
+		
+		try {
+			fatura.checaValor(fatura.getValor());
+		} catch (RuntimeException e) {
+			Assertions.assertEquals("Valor nulo!", e.getMessage());
+		}
+    }
 }
